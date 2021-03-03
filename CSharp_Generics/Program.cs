3b21aa7b337decc10ed671699cca12d7aec6d099
Kiran Kumar Balasubramanian <kbalasubramanian@pkglobal.com>
2021-03-04 01:28:38 +0530

    Add project files.

---
+++ CSharp_Generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
